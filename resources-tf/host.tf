f51e2d0650179af9de76206b6a78b808ed72fc77
Tomáš Virtus <tomas.virtus@rchain.coop>
2019-04-16 23:15:35 +0200

    Initial commit

---
+++ resources-tf/host.tf
@@ -0,0 +1,66 @@
+resource "google_compute_address" "ext_addr" {
+  name = "ethereum-node"
+  address_type = "EXTERNAL"
+}
+
+resource "google_dns_record_set" "dns_record" {
+  name = "eth.rchain-dev.tk."
+  managed_zone = "rchain-dev"
+  type = "A"
+  ttl = 300
+  rrdatas = ["${google_compute_address.ext_addr.address}"]
+}
+
+resource "google_compute_disk" "data_disk" {
+  name = "ethereum-node-data"
+  type = "pd-standard"
+  size = 4096
+}
+
+resource "google_compute_instance" "host" {
+  name = "ethereum-node"
+  hostname = "eth.rchain-dev.tk"
+  machine_type = "n1-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1810"
+      size = 20
+      type = "pd-standard"
+    }
+  }
+
+  attached_disk {
+    source = "${google_compute_disk.data_disk.self_link}"
+    device_name = "ethereum-node-data"
+  }
+
+  tags = [
+    "ethereum",
+    "collectd-out",
+  ]
+
+  network_interface {
+    network = "${data.google_compute_network.default_network.self_link}"
+    access_config {
+      nat_ip = "${google_compute_address.ext_addr.address}"
+    }
+  }
+
+  depends_on = [ "google_dns_record_set.dns_record" ]
+
+  connection {
+    type = "ssh"
+    user = "root"
+    private_key = "${file("~/.ssh/google_compute_engine")}"
+  }
+
+  provisioner "file" {
+    source = "${var.rchain_sre_git_crypt_key_file}"
+    destination = "/root/rchain-sre-git-crypt-key"
+  }
+
+  provisioner "remote-exec" {
+    script = "../bootstrap"
+  }
+}
---
+++ resources-tf/network.tf
@@ -0,0 +1,29 @@
+data "google_compute_network" "default_network" {
+  name = "default"
+}
+
+resource "google_compute_firewall" "fw_rpc" {
+  name = "ethereum-rpc"
+  network = "${data.google_compute_network.default_network.self_link}"
+  priority = 510
+  target_tags = [ "ethereum" ]
+  allow {
+    protocol = "tcp"
+    ports = [ 8545, 8546 ]
+  }
+}
+
+resource "google_compute_firewall" "fw_p2p" {
+  name = "ethereum-p2p"
+  network = "${data.google_compute_network.default_network.self_link}"
+  priority = 520
+  target_tags = [ "ethereum" ]
+  allow {
+    protocol = "tcp"
+    ports = [ 30303 ]
+  }
+  allow {
+    protocol = "udp"
+    ports = [ 30303, 30304 ]
+  }
+}
